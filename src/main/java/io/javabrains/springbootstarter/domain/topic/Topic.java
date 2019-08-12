791b7bfba4828dd0a1e013eaf982fc0669a9556c
Nazar Lelyak <nazar.lelyak@chartermatch.com>
2019-08-12 18:28:59 +0300

    add more meta information

--- src/main/java/io/javabrains/springbootstarter/domain/topic/Topic.java
+++ src/main/java/io/javabrains/springbootstarter/domain/topic/Topic.java
@@ -1,5 +1,7 @@
 package io.javabrains.springbootstarter.domain.topic;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,16 +11,21 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Data
+@Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@ApiModel(description = "Details about the topic")
 public class Topic {
 
 	@Id
+	@ApiModelProperty(notes = "The unique id of the topic")
 	private String id;
-	private String name;
-	private String description;
 
+	@ApiModelProperty(notes = "the topic's name")
+	private String name;
+
+	@ApiModelProperty(notes = "the topic's description")
+	private String description;
 
 }
--- src/main/java/io/javabrains/springbootstarter/domain/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/domain/topic/TopicController.java
@@ -1,7 +1,15 @@
 package io.javabrains.springbootstarter.domain.topic;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -11,27 +19,41 @@ public class TopicController {
 
     private TopicService topicService;
 
-    @RequestMapping(value = "/topics", method = RequestMethod.GET)
+    @ApiOperation(value = "Find All Topics",
+            responseContainer = "List",
+            response = Topic.class)
+    @GetMapping(value = "/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
-    @RequestMapping(value = "/topics/{id}", method = RequestMethod.GET)
-    public Topic getTopic(@PathVariable String id) {
+
+    @ApiOperation(value = "Find Topics by ID",
+            notes = "Provide an id to look up specific topic",
+            response = Topic.class)
+    @GetMapping(value = "/topics/{id}")
+    public Topic getTopic(@ApiParam(value = "ID value for the contact you need to retrieve", required = true)
+                          @PathVariable String id) {
         return topicService.getTopic(id);
     }
 
-    @RequestMapping(value = "/topics", method = RequestMethod.POST)
+    @ApiOperation(value = "Add new Topic",
+            notes = "Provide an topic to be added in a body")
+    @PostMapping(value = "/topics")
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
     }
 
-    @RequestMapping(value = "/topics/{id}", method = RequestMethod.PUT)
+    @ApiOperation(value = "Update a Topic",
+            notes = "Provide an id of topic for update, and new representation of topic")
+    @PutMapping(value = "/topics/{id}")
     public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
         topicService.updateTopic(id, topic);
     }
 
-    @RequestMapping(value = "/topics/{id}", method = RequestMethod.DELETE)
+    @ApiOperation(value = "Add new Topic",
+            notes = "Provide an topic's id")
+    @DeleteMapping(value = "/topics/{id}")
     public void deleteTopic(@PathVariable String id) {
         topicService.deleteTopic(id);
     }
